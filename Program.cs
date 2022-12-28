856d7af11c98249d144c37598a5f05b9b9fb0efb
Ghanem225 <121583227+Ghanem225@users.noreply.github.com>
2022-12-29 00:44:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project
+{
+    public class PerfectNumber
+    {
+        public static void Main()
+        {
+            int d, q, sum;
+            int start, end;
+
+            Console.Write("Start : ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("End : ");
+            end = int.Parse(Console.ReadLine());
+
+            for (d = start; d <= end; d++)
+            {
+                q = 1;
+                sum = 0;
+                while (q < d)
+                {
+                    if (d % q == 0)
+                        sum = sum + q;
+                    q++;
+                }
+                if (sum == d)
+                    Console.WriteLine(d);
+            }
+        }
+    }
+}
